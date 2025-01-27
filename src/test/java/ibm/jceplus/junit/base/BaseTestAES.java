16d7f0ff959801a473a611eb3dd96cb708963785
Tao Liu <tao.liu@ibm.com>
2025-01-27 13:41:42 -0500

    update

--- src/test/java/ibm/jceplus/junit/base/BaseTestAES.java
+++ src/test/java/ibm/jceplus/junit/base/BaseTestAES.java
@@ -28,7 +28,7 @@ import javax.crypto.spec.RC5ParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -1074,11 +1074,12 @@ public class BaseTestAES extends BaseTestCipher {
         }
     }
 
-    @Test
-    // @RepeatedTest(1000)
+    // @Test
+    @RepeatedTest(1000)
     public void testUpdateForAES_CBC_PKCS5Padding() throws Exception {
 
         try {
+            System.out.println("Tao Debug - Test");
             byte[] iv = new byte[16];
             Arrays.fill(iv, (byte) 0);
             IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
