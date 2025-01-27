6fb9cc1ffc019cc27d0d389983370c74941ed6a0
Tao Liu <tao.liu@ibm.com>
2025-01-27 15:35:13 -0500

    update

--- src/test/java/ibm/jceplus/junit/base/BaseTestAES.java
+++ src/test/java/ibm/jceplus/junit/base/BaseTestAES.java
@@ -258,13 +258,13 @@ public class BaseTestAES extends BaseTestCipher {
         }
     }
 
-    @RepeatedTest(1000)
+    // @RepeatedTest(1000)
     public void testAES_ECB_NoPadding() throws Exception {
         System.out.println("Tao Debug 20");
         encryptDecrypt("AES/ECB/NoPadding", true, false);
     }
 
-    // @RepeatedTest(1000)
+    @RepeatedTest(1000)
     public void testAES_ECB_PKCS5Padding() throws Exception {
         System.out.println("Tao Debug 21");
         encryptDecrypt("AES/ECB/PKCS5Padding");
